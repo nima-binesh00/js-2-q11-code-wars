0f7ee569a4ff4fb6a49b15373729017f3265db25
nima-binesh00 <nimabinesh00@gmail.com>
2024-12-12 11:52:30 +0330

    add-homework

---
+++ js11-codewars/codeWars.js
@@ -0,0 +1,74 @@
+//* Code wars ==================================================================
+// Complete the following CodeWars exercises.
+
+//1-Get key/value pairs as arrays
+//https://www.codewars.com/kata/515dfd2f1db09667a0000003
+
+//2- Convert number to reversed array of digits
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+
+//3- Training JS #5: Basic data types--Object
+// https://www.codewars.com/kata/571f1eb77e8954a812000837
+
+//4-Object to array👺
+// https://www.codewars.com/kata/58666e5811ceec2fa5000060
+
+//5- Count of positives / sum of negatives😎
+// https://www.codewars.com/kata/576bb71bbbcf0951d5000044
+
+//6- Removing Elements😪
+// https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
+
+//7- Combine objects😪
+// https://www.codewars.com/kata/56bd9e4b0d0b64eaf5000819
+
+//8- Find the Difference in Age between Oldest and Youngest Family Members
+// https://www.codewars.com/kata/5720a1cb65a504fdff0003e2
+
+//9- Crash Override
+// https://www.codewars.com/kata/578c1e2edaa01a9a02000b7f
+// function combine(ab) {
+//   // Your code here
+//   let a = ab[0];
+//   console.log(a);
+
+//   let b = ab[1];
+
+//   for (const key in a) {
+//     for (const key2 in b) {
+//       if (key == key2) {
+//         a[key] += b[key2];
+//       }
+//     }
+//   }
+//   return a;
+// }
+
+// function arrayToObject(arr) {
+//   let num = 0;
+//   let obj = {};
+//   let num1;
+//   for (const element of arr) {
+//     if (num % 2 == 0) {
+//       num1 = element;
+//       num++;
+//     } else {
+//       obj[num1] = element;
+//       num++;
+//     }
+//   }
+//   return obj;
+// }
+// console.log(arrayToObject([1, 1, 2, 2, 3, 3]));
+
+// function objectToArray(obj) {
+//   let num44 = [];
+//   for (const key in obj) {
+//     for (let num = 1; num <= obj[key]; num++) {
+//       num44.push(+key);
+//     }
+//   }
+
+//   return num44;
+// }
+// console.log(objectToArray({ 2: 5, 99: 1 }));
